e423809b131f041ba21b40e950f1f2048c8e5f13
Rcuhi <rappinngu2006@gmail.com>
2024-01-30 21:26:50 +0900

    無限スクロールのテスト

--- components/Main.tsx
+++ components/Main.tsx
@@ -21,14 +21,14 @@ export default function Main() {
                   <span className="sr-only">Open sidebar</span>
                   <svg
                     className="w-6 h-6"
-                    aria-hidden="true"
+                    aria-hidden
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
+                      clipRule="evenodd"
+                      fillRule="evenodd"
                       d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
                     ></path>
                   </svg>
@@ -65,7 +65,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 18 21"
@@ -77,7 +77,7 @@ export default function Main() {
                 </span>
                 <svg
                   className="w-3 h-3"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 10 6"
@@ -125,7 +125,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 18 18"
@@ -145,7 +145,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -165,7 +165,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 20 18"
@@ -182,7 +182,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 18 20"
@@ -199,16 +199,16 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 18 16"
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
                   />
                 </svg>
@@ -222,7 +222,7 @@ export default function Main() {
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
+                  aria-hidden
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 20 20"
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import Image from "next/image";
 
-export default function Thumbnail() {
+type Props = {
+  title: string;
+};
+
+export default function Thumbnail({ title }: Props) {
   return (
-    <div className="max-w-sm bg-white dark:bg-gray-800 dark:border-gray-700">
+    <div
+      className="max-w-sm bg-white dark:bg-gray-800 dark:border-gray-700"
+      key={title}
+    >
       <a href="#">
         <Image src={"/hqdefault.jpg"} width={480} height={360} alt=""></Image>
       </a>
       <p className="my-1 ml-2 font-normal text-gray-700 dark:text-gray-400">
-        タイトルですよー
+        {title}
       </p>
     </div>
   );

c694217ff24b5e61dd63b1d95e2871dd6a74e8c0
Rcuhi <rappinngu2006@gmail.com>
2024-03-13 22:54:41 +0900

    ヘッダーの見た目を変更

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,36 +1,87 @@
-import { AppBar, Button, Container, Link, Stack, Toolbar } from "@mui/material";
+// import SearchIcon from "@mui/icons-material/Search";
+import { AppBar, Button, Link, Toolbar, styled } from "@mui/material";
+// import InputBase from "@mui/material/InputBase";
+// import { alpha, styled } from "@mui/material/styles";
 import Image from "next/image";
+
+export const NavButton = styled(Button)({
+  color: "primary",
+  fontWeight: "bold",
+});
+
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginLeft: 0,
+//   width: "100%",
+//   [theme.breakpoints.up("sm")]: {
+//     marginLeft: theme.spacing(1),
+//     width: "auto",
+//   },
+// }));
+
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
+
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "inherit",
+//   width: "100%",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     [theme.breakpoints.up("sm")]: {
+//       width: "12ch",
+//       "&:focus": {
+//         width: "20ch",
+//       },
+//     },
+//   },
+// }));
 
 export default function Navbar() {
   return (
     <AppBar position="static" color="default">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Link href="/" sx={{ flexGrow: 1, marginTop: 1, marginBottom: 0.5 }}>
-            <Image
-              src="/MAP.png"
-              alt="Music Archives Project Logo"
-              width={240}
-              height={60}
+      <Toolbar>
+        <Link href="/" sx={{ flexGrow: 1, marginTop: 1, marginBottom: 0.5 }}>
+          <Image
+            src="/MAP.png"
+            alt="Music Archives Project Logo"
+            width={240}
+            height={60}
+          />
+        </Link>
+        {/* <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
             />
-          </Link>
-          <Stack direction="row" spacing={2} sx={{ m: 2, p: 2 }}>
-            <Button variant="contained" color="primary" href="/#">
-              トップ
-            </Button>
-            <Button variant="contained" color="primary" href="/#">
-              サービス
-            </Button>
-            <Button
-              variant="contained"
-              href="https://forms.gle/osqdRqh1MxWhA51A8"
-              target="_blank"
-            >
-              お問い合わせ
-            </Button>
-          </Stack>
-        </Toolbar>
-      </Container>
+          </Search> */}
+        <NavButton color="primary" href="/#">
+          トップ
+        </NavButton>
+        <NavButton color="primary" href="/#">
+          サービス
+        </NavButton>
+        <NavButton href="https://forms.gle/osqdRqh1MxWhA51A8" target="_blank">
+          お問い合わせ
+        </NavButton>
+      </Toolbar>
     </AppBar>
     // <nav className="fixed top-0 z-50 min-h-18 w-full border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
     //   <div className="min-h-18 justify-between p-3 md:flex">

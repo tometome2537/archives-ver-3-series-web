7d6f9921580443648d56acf4a39a3c902a76912c
Rcuhi <rappinngu2006@gmail.com>
2024-04-02 23:06:24 +0900

    動画の数を取得するメソッドをsupabase-jsで実装

--- app/api/videos/count/route.ts
+++ app/api/videos/count/route.ts
@@ -1,4 +1,5 @@
-import prisma from "@/libs/prisma.helper";
+import "@/libs/supabase.count";
+import supabase from "@/libs/supabase.helper";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -9,26 +10,32 @@ export async function GET(request: NextRequest) {
     const qSearch = searchParams.get("search");
     const search = qSearch || null;
 
-    const count = await prisma.video.count({
-      where: search ? {
-        OR: [
-          {
-            title: {
-              search: search
-            }
-          },
-          {
-            description: {
-              search: search
-            }
-          }
-        ]
-      } : {},
-    });
+    const count = await supabase.count("Video");
+
+    // if (error) throw error;
 
     return NextResponse.json(count, {
       status: 200,
     });
+
+    // const count = await prisma.video.count({
+    //   where: search ? {
+    //     OR: [
+    //       {
+    //         title: {
+    //           search: search
+    //         }
+    //       },
+    //       {
+    //         description: {
+    //           search: search
+    //         }
+    //       }
+    //     ]
+    //   } : {},
+    // });
+
+
   } catch (error) {
     console.log(error);
     return err;

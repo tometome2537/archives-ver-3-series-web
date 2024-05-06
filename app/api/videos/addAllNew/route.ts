dfa7c12f557bc0b5768176fc63435d0bb054e1e0
Ruchi12377 <rappinngu2006@gmail.com>
2024-05-07 00:14:34 +0900

    レスポンスの変更とコメントの追加など

--- app/api/videos/addAllNew/route.ts
+++ app/api/videos/addAllNew/route.ts
@@ -5,14 +5,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const { data: channels, error: chError } = await supabase.from("YouTubeAccount").select("id, relatedPlaylistsUploads");
-    const playlists = channels?.filter(x => x.id == "UCZx7esGXyW6JXn98byfKEIA");
+    const { searchParams } = request.nextUrl;
+    const qChannelId = searchParams.get("channelId");
+    // とりあえずぷらそにかのみ
+    if (qChannelId == null) return NextResponse.json({ error: "error" });
 
+    const playlists = channels?.filter(x => x.id == qChannelId);
+
+    // // 見つからなかった
     if (playlists == undefined) return NextResponse.json(chError);
+    if (playlists.length == 0) return NextResponse.json({
+        status: 200,
+        statusText: "success",
+        details: {
+            message: "プレイリストが見つかりませんでした。"
+        }
+    });
+    const addedVideos = [];
 
+    // 各チャンネルの全動画が含まれている再生リストから新着動画を探す
     for (let i = 0; i < playlists.length; i++) {
+        // プレイリストからすべての動画を取得する
         const { relatedPlaylistsUploads } = playlists[i];
         const { result, status, statusText } = await getPlaylistItems(relatedPlaylistsUploads);
 
+        // 失敗してたら次のチャンネルへ
         if (status != 200) continue;
 
         for (let j = 0; j < result.length; j++) {
@@ -27,6 +44,7 @@ export async function GET(request: NextRequest) {
             // countが0より大きい→データが存在する場合は追加をスキップする
             if (count ?? 0 > 0) continue;
 
+            // 必要なデータをよしなに作成
             const videoData = {
                 "id": item.snippet?.resourceId?.videoId ?? "",
                 "categoryChecked": false,
@@ -46,12 +64,28 @@ export async function GET(request: NextRequest) {
                 "duration": 0,
             }
 
+            // 新しいデータを追加する
             const { error } = await supabase.from("Video").insert(videoData);
-            if (error) {
-                return NextResponse.json(error);
+
+            // 失敗してないなら続ける
+            if (error == null) {
+                addedVideos.push(videoData.id);
+                continue;
             }
+
+            // 失敗したら即エラーを吐く
+            return NextResponse.json({
+                status: error.code,
+                statusText: error.message,
+                details: {
+                    error: {
+                        details: error.details,
+                        hint: error.hint
+                    }
+                }
+            });
         }
     }
 
-    return NextResponse.json({ status: "success" });
+    return NextResponse.json({ status: 200, statusText: "success", details: addedVideos });
 }

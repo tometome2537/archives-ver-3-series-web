1a97dd97082a37dd79e7d13a63a16439bd486aaf
とめとめ <146919125+tometome2537@users.noreply.github.com>
2024-12-26 00:05:07 +0900

    アーティスト情報を取得できるように。

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/vi/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };

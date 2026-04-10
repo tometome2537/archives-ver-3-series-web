7c53b2d562b7c0b09b6ba655ff75d4f336297682
tometome2537 <146919125+tometome2537@users.noreply.github.com>
2026-04-10 20:22:03 +0900

    3.3.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 import { AppleMusicProvider } from "@/contexts/AppleMusicContext";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 
 const inter = Inter({ subsets: ["latin"] });
 

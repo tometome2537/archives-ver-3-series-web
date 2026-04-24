845327d91a2dd73557c7cd4d23824164d3dad5b1
とめとめ <146919125+tometome2537@users.noreply.github.com>
2026-04-25 00:55:21 +0900

    微修正

--- next.config.js
+++ next.config.js
@@ -56,12 +56,14 @@ module.exports = withSentryConfig(module.exports, {
   // side errors will fail.
   tunnelRoute: "/monitoring",
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  // ✔ 新方式（重要）
+  webpack: (config) => {
+    config.treeshake = {
+      removeDebugLogging: true,
+    };
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+    config.automaticVercelMonitors = true;
+
+    return config;
+  },
 });
